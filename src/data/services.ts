8bdcd66bf6a120b1006749fd89757b5710e40491
Shoxruh Tuxtanazarov <stuk@Mac.home>
2026-04-07 08:58:21 +0500

    edit

--- src/data/services.ts
+++ src/data/services.ts
@@ -1,5 +1,18 @@
 export type ServiceType = 'hotel' | 'restaurant' | 'place';
 export type Region = 'Termiz' | 'Moynaq' | 'Khiva' | 'Kokand' | 'Namangan' | 'Zaamin' | 'Samarkand' | 'Shahrisabz' | 'Bukhara' | 'Tashkent';
+import chimyon from "../Img/unnamed.jpg";
+import amirsoy from '../Img/amirsoy-gondola.jpg';
+import balandchaqir from '../Img/Balandchaqir.jpg';
+import zomin from '../Img/zomin.webp';
+import achchiqKol from '../Img/images.jpg';
+import sangardak from '../Img/Sangardak_sharsharasi_-_30.jpg';
+import baxmal from '../Img/baxmal.jpg';
+import suvtushar from '../Img/Suvtushar_sharsharasi_Shahrisabz.jpg';
+import aydarkol from '../Img/aydarko`l.jpg';
+import xojamayxona from '../Img/xo`jamayxona.jpg';
+import albarsoy from '../Img/albarsoy.webp';
+import xojamushkent from '../Img/f1bb905744cc466c61ef3a5b570ea81e.jpg'
+
 
 export interface Service {
   id: string;
@@ -24,7 +37,7 @@ export const SERVICES: Service[] = [
     region: 'Tashkent',
     type: 'place',
     rating: 4.8,
-    image: 'https://images.unsplash.com/photo-1549487563-fb651e604f37?auto=format&fit=crop&q=80&w=1000',
+    image: chimyon,
     description: {
       ru: 'Прекрасный горный курорт недалеко от Ташкента, идеален для зимнего и летнего отдыха.',
       en: 'A beautiful mountain resort near Tashkent, ideal for winter and summer holidays.',
@@ -42,7 +55,7 @@ export const SERVICES: Service[] = [
     region: 'Tashkent',
     type: 'place',
     rating: 4.9,
-    image: 'https://images.unsplash.com/photo-1551698618-1dfe5d97d256?auto=format&fit=crop&q=80&w=1000',
+    image: amirsoy,
     description: {
       ru: 'Современный горнолыжный курорт мирового уровня.',
       en: 'Modern world-class ski resort.',
@@ -60,7 +73,7 @@ export const SERVICES: Service[] = [
     region: 'Namangan',
     type: 'place',
     rating: 4.7,
-    image: 'https://images.unsplash.com/photo-1580637250188-062e17707f45?auto=format&fit=crop&q=80&w=1000',
+    image: xojamushkent,
     description: {
       ru: 'Историческое и святое место, окруженное красивой природой.',
       en: 'Historical and sacred place surrounded by beautiful nature.',
@@ -78,7 +91,7 @@ export const SERVICES: Service[] = [
     region: 'Zaamin',
     type: 'place',
     rating: 4.6,
-    image: 'https://images.unsplash.com/photo-1542314831-068cd1dbfeeb?auto=format&fit=crop&q=80&w=1000',
+    image: balandchaqir,
     description: {
       ru: 'Красивое природное место.',
       en: 'Beautiful natural spot.',
@@ -96,7 +109,7 @@ export const SERVICES: Service[] = [
     region: 'Zaamin',
     type: 'place',
     rating: 4.9,
-    image: 'https://images.unsplash.com/photo-1540962351504-03099e0a754b?auto=format&fit=crop&q=80&w=1000',
+    image: zomin,
     description: {
       ru: 'Узбекская Швейцария с хвойными лесами и чистейшим воздухом.',
       en: 'Uzbek Switzerland with coniferous forests and purest air.',
@@ -114,7 +127,7 @@ export const SERVICES: Service[] = [
     region: 'Khiva',
     type: 'place',
     rating: 4.5,
-    image: 'https://images.unsplash.com/photo-1566073771259-6a8506099945?auto=format&fit=crop&q=80&w=1000',
+    image: achchiqKol,
     description: {
       ru: 'Живописное озеро, идеальное для отдыха.',
       en: 'Picturesque lake, ideal for relaxation.',
@@ -132,7 +145,7 @@ export const SERVICES: Service[] = [
     region: 'Termiz',
     type: 'place',
     rating: 4.8,
-    image: 'https://images.unsplash.com/photo-1528150177508-7cc0c36cda5c?auto=format&fit=crop&q=80&w=1000',
+    image: sangardak,
     description: {
       ru: 'Один из самых красивых водопадов Узбекистана.',
       en: 'One of the most beautiful waterfalls in Uzbekistan.',
@@ -150,7 +163,7 @@ export const SERVICES: Service[] = [
     region: 'Zaamin',
     type: 'place',
     rating: 4.7,
-    image: 'https://images.unsplash.com/photo-1623851502447-19076046e7f8?auto=format&fit=crop&q=80&w=1000',
+    image: baxmal,
     description: {
       ru: 'Горный район с уникальной природой и яблоневыми садами.',
       en: 'Mountainous area with unique nature and apple orchards.',
@@ -164,11 +177,11 @@ export const SERVICES: Service[] = [
   },
   {
     id: '9',
-    title: { ru: 'Сувтушар', en: 'Suvtushar', uz: 'Suvtishlar' },
+    title: { ru: 'Сувтушар', en: 'Suvtushar', uz: 'Suvtushar' },
     region: 'Shahrisabz',
     type: 'place',
     rating: 4.6,
-    image: 'https://images.unsplash.com/photo-1542314831-068cd1dbfeeb?auto=format&fit=crop&q=80&w=1000',
+    image: suvtushar,
     description: {
       ru: 'Красивый водный источник в горах.',
       en: 'Beautiful water spring in the mountains.',
@@ -186,7 +199,7 @@ export const SERVICES: Service[] = [
     region: 'Zaamin',
     type: 'place',
     rating: 4.8,
-    image: 'https://images.unsplash.com/photo-1582719478250-c89cae4dc85b?auto=format&fit=crop&q=80&w=1000',
+    image: aydarkol,
     description: {
       ru: 'Огромное озеро посреди пустыни, прекрасное место для рыбалки и отдыха.',
       en: 'A huge lake in the middle of the desert, a beautiful place for fishing and recreation.',
@@ -204,7 +217,7 @@ export const SERVICES: Service[] = [
     region: 'Termiz',
     type: 'place',
     rating: 4.7,
-    image: 'https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?auto=format&fit=crop&q=80&w=1000',
+    image: xojamayxona,
     description: {
       ru: 'Целебный источник в живописном ущелье.',
       en: 'Healing spring in a picturesque gorge.',
@@ -222,7 +235,7 @@ export const SERVICES: Service[] = [
     region: 'Tashkent',
     type: 'place',
     rating: 4.6,
-    image: 'https://images.unsplash.com/photo-1580637250188-062e17707f45?auto=format&fit=crop&q=80&w=1000',
+    image: albarsoy,
     description: {
       ru: 'Горная река с красивыми видами.',
       en: 'Mountain river with beautiful views.',
